e125ce751872f9b8b69e1aec31f9fc13a2f987cd
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-02-19 16:02:34 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import axios from 'axios';
+
+
+export default function CountrySelect() {
+  let result = ''
+
+  async function request() {
+    const data = await axios.get('https://www.nbrb.by/API/ExRates/Currencies')
+    console.log(data);
+    result = data.Cur_Name;
+    return result
+  }
+
+  return (
+    <Autocomplete
+      id="country-select-demo"
+      sx={{ width: 300 }}
+      options={request}
+      autoHighlight
+      getOptionLabel={(option) => option.label}
+      renderOption={(props, option) => (
+        <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+          <img
+            loading="lazy"
+            width="20"
+            src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+            srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+            alt=""
+          />
+          {option.label} ({option.code}) +{option.phone}
+        </Box>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Choose a country"
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: 'new-password', // disable autocomplete and autofill
+          }}
+        />
+      )}
+    />
+  );
+}
